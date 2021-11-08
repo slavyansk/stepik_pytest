93bf0fa15877d1d787794a8cf2f7bb1fd6fff151
slavyansk <74851625+slavyansk@users.noreply.github.com>
2021-11-08 21:35:36 +0300

    add time.sleep()

--- test_items.py
+++ test_items.py
@@ -3,6 +3,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_should_see_btn_add_to_basket(browser):
     browser.get(link)
+    time.sleep(30)
     browser.find_element_by_class_name('.btn-add-to-basket')
 
     
