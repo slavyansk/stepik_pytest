254189abab42c2d85b921741fbe240d88def91d6
unknown <slavyansk2001@mail.ru>
2021-11-09 16:37:28 +0300

    add Assert

--- test_items.py
+++ test_items.py
@@ -6,7 +6,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_should_see_btn_add_to_basket(browser):
     browser.get(link)
-    time.sleep(30)
-    browser.find_element_by_class_name('.btn-add-to-basket')
+    time.sleep(10)
+    button = browser.find_element_by_class_name('btn-add-to-basket')
+    assert button, "No add to basket button"
 
-    
+
